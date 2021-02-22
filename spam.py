a74d7a084960290414a53215f8e9a10cfae656df
MohammedAl-Rasheed <69544031+MohammedAl-Rasheed@users.noreply.github.com>
2021-02-21 23:09:34 -0500

    Update spam.py

--- spam.py
+++ spam.py
@@ -18,3 +18,17 @@ driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
 sleep(2)
 driver.get("https://mail.google.com")
 sleep(2)
+
+def speed(j):
+  driver.get("https://mail.google.com/mail/u/0/#inbox?compose=new")
+  sleep(3)
+  driver.find_element_by_xpath("//textarea[@name='to']").send_keys("speedmero@gmail.com")
+  driver.find_element_by_xpath("//input[@name='subjectbox']").send_keys("yo it worked btw " + str(j))
+  driver.find_element_by_css_selector("div[aria-label='Message Body']").send_keys("yo it worked btw " + str(j))
+  driver.find_element_by_xpath("//div[text()='Send']").click()
+  sleep(3)
+
+for i in range(10):
+  j = i + 1
+  speed(j)
+  sleep(3)
